948f9d6ca79ae3ab7764724c70fe4ec3ab0373f5
Palash Mishra <palashmishra54@gmail.com>
2021-02-15 15:36:18 +0530

    Comments Added

--- armstrong.c
+++ armstrong.c
@@ -3,18 +3,18 @@ int main()
 {
     int n,rem,sum=0,no;
     printf("\nENTER NO. :");
-    scanf("%d",&n);
-    no=n;
-    while(n)
+    scanf("%d",&n);//Taking user's input.
+    no=n;//Making a copy of Input.
+    while(n)//Main logic of the code.
     {
         rem=n%10;
         n=n/10;
         sum+=rem*rem*rem;
     }
-    if(no==sum)
-        printf("\nARMSTRONG NO.!");
+    if(no==sum)//Checking the conditon belongs to the logic.
+        printf("\nARMSTRONG NO.!");//print if true.
     else
-        printf("\nOOPS!NOT AN ARMSTRONG NO!");
+        printf("\nOOPS!NOT AN ARMSTRONG NO!");//print if false.
     return 0;
 
 }
